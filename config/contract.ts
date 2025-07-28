bd7c9b1d8dd3547b579e9bb262d3f28756fb1d9f
Austin Ameh <arewageek@gmail.com>
2025-07-29 00:15:49 +0100

    Update contract address to use environment variable and remove unused chain import

--- config/contract.ts
+++ config/contract.ts
@@ -2,7 +2,7 @@ import creativops from "@/abi/creativops.json";
 import { monadTestnet } from "viem/chains";
 
 export const contractConfig = {
-  address: "0x546592467bDCa9118Ab52856eEbb5211c1Af3900",
+  address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string,
   abi: creativops.abi,
   chainId: monadTestnet.id,
 } as const;
